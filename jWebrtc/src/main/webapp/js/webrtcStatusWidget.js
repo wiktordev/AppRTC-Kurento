474a0fce7fa7367908b79450c3d2e79f80d6c07b
Nico Krause <inspiraluna@gmail.com>
2016-09-02 13:38:23 +0500

    improved webrtcStatusWidget.js

--- jWebrtc/src/main/webapp/js/webrtcStatusWidget.js
+++ jWebrtc/src/main/webapp/js/webrtcStatusWidget.js
@@ -1,7 +1,8 @@
 //import dependencies
-var server = window.location.host; //location.host;
-if(server==null || server=="" || server.lastIndexOf ("file:")>-1) server = "webrtc.a-fk.de";
-console.log('server: '+server);
+//var server = window.location.host; //location.host;
+//if(server==null || server=="" || server.lastIndexOf ("file:")>-1) 
+var server = "webrtc.a-fk.de";
+//console.log('server: '+server);
 
 document.write("<script type='text/javascript' src='https://" + server + "/jWebrtc/bower_components/jquery/dist/jquery.min.js'></script>"); 
 document.write("<script type='text/javascript' src='https://" + server + "/jWebrtc/bower_components/bootstrap/dist/js/bootstrap.min.js'></script>"); 
